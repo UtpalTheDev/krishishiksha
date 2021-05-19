3e913d06ab15a3131ff6d7fca914bbaebda346ba
Utpal Pati <utpalpati8@gmail.com>
2021-05-19 20:14:52 +0000

    refactor: appbar added

--- quiz/src/Navbar.tsx
+++ quiz/src/Navbar.tsx
@@ -1,12 +1,23 @@
 import { useReduce } from "./Reducer-context";
 import { useLogin } from "./Login-context";
 import { Link } from "react-router-dom";
-
+import{AppBar,Toolbar,IconButton,Avatar,Grid} from "@material-ui/core";
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
 export function Navbar() {
   return (
     <>
       <div>
-        <Link to="/user">user</Link>
+        
+        <AppBar>
+          <Toolbar>
+            <Grid container justify="space-between" alignItems="center">
+            <h3>Quiz</h3>
+            <IconButton href="/user">
+              <AccountCircleRoundedIcon/>
+            </IconButton>
+            </Grid>
+          </Toolbar>
+        </AppBar>
       </div>
     </>
   );

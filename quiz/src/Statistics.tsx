2b3e3751919c672440afca47a64344d67f16d39c
Utpal Pati <utpalpati8@gmail.com>
2021-05-20 07:58:54 +0000

    style: center aligned

--- quiz/src/Statistics.tsx
+++ quiz/src/Statistics.tsx
@@ -1,12 +1,14 @@
 import { useReduce } from "./Reducer-context";
-
+import {Box} from "@material-ui/core"
 export function Statistics() {
   let { correct, wrong, data } = useReduce();
   return (
-    <>
+    <><Box textAlign="center">
       <p> Attempted- {correct + wrong}</p>
       <p> Correct ans- {correct}</p>
       <p> Wrong ans- {wrong}</p>
+    </Box>
     </>
+
   );
 }

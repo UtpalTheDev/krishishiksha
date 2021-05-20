9068c06999ea3184e697a50ee088e3fcfc8b2b3b
Utpal Pati <utpalpati8@gmail.com>
2021-05-20 07:56:20 +0000

    refactor: error state add + fix: uselocation route

--- quiz/src/Login.tsx
+++ quiz/src/Login.tsx
@@ -3,7 +3,7 @@ import { useLogin } from "./Login-context";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios, { AxiosError } from "axios";
-import { Location } from "history";
+import Location  from "history";
 
 import {Button,Box,Grid,TextField} from "@material-ui/core";
 
@@ -11,15 +11,17 @@ import {Button,Box,Grid,TextField} from "@material-ui/core";
 export function Login() {
   let { dispatch, user } = useReduce();
   let { isUserLogin, setLogin } = useLogin();
-
-  let from = useLocation();
+  const [Error,setError]=useState(false);
+  let {state,pathname} = useLocation();
   let navigate = useNavigate();
 
   let [email, setemail] = useState("");
   let [password, setpassword] = useState("");
+
+  console.log("navigation",state);
   function navigationcall() {
     if (isUserLogin) {
-      navigate("/home");
+      navigate(state!==null ? state : "/home");
     }
   }
   useEffect(() => {
@@ -27,13 +29,17 @@ export function Login() {
   });
 
   // navigationcall();
-
+console.log("error",Error);
   function emailhandler(event: React.ChangeEvent<HTMLInputElement>) {
     setemail(event.target.value);
   }
   function passwordhandler(event: React.ChangeEvent<HTMLInputElement>) {
     setpassword(event.target.value);
   }
+
+    
+    
+  
 
   type User = {
     _id: string;
@@ -50,8 +56,13 @@ export function Login() {
         "https://quiz-backend-demo-1.utpalpati.repl.co/user/infowithcred",
         { email: email, password: password }
       );
+      console.log("verify success",response)
+      setError(false);
       return response.data;
     } catch (error) {
+      console.log("verify error")
+
+      setError(true);
       if (axios.isAxiosError(error)) {
         let servererror = error as AxiosError<Servererror>;
         if (servererror && servererror.response) {
@@ -62,8 +73,10 @@ export function Login() {
     }
   }
   async function userassign() {
+    if(email!==""&&password!==""){
     let userdata = await verify();
-    if ("name" in userdata) {
+    userdata===null?setError(true):setError(false)
+    if (userdata!==null && "name" in userdata) {
       localStorage.setItem(
         "user",
         JSON.stringify({ userid: userdata._id, login: true })
@@ -73,17 +86,27 @@ export function Login() {
     }
     console.log(userdata);
   }
+  }
   return (
     <>
       <Box>
         <h1>login</h1>
+
         <Grid container 
         direction="column"
         style={{marginBottom:"1rem"}}justify="center" alignItems="center" spacing={1}>
-        <Grid item>    <TextField id="standard-basic" value={email}  label="Email Id" onChange={emailhandler}/></Grid>  
+        <Grid item>    
+        <TextField 
+        id="standard-basic" 
+        error={Error}
+        value={email}  
+        label="Email Id"
+         onChange={emailhandler}/>
+         </Grid>  
     
         <Grid item>
         <TextField
+        error={Error}
           label="Password"
           value={password}
           onChange={passwordhandler}
@@ -91,6 +114,7 @@ export function Login() {
         <Grid item>
         <Button
         style={{marginTop:"1rem"}}
+        
           size="small"
           color="primary"
           variant="contained"

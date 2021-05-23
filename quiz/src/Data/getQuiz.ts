2f32e04f24e83707cc69c43d60e32a106c53abe9
Utpal Pati <utpalpati8@gmail.com>
2021-05-23 09:57:45 +0000

    refactor: data added

--- quiz/src/Data/getQuiz.ts
+++ quiz/src/Data/getQuiz.ts
@@ -1,70 +1,10 @@
 import { quiz, Quizdata } from "./quiz.types";
 
 export const quizdata: Quizdata = {
-  "Marvel": {
-    quizName: "Marvel Cinematic Universe",
-    questions: [
-      {
-        question:
-          "How many avengers were there in first Avengers movie released in 2012?",
-        points: 5,
-        options: [
-          {
-            text: "22",
-            isRight: false
-          },
-          {
-            text: "6",
-            isRight: true
-          }
-        ]
-      },
-      {
-        question:
-          "What was Dr. Strange doing during the fight of New York in 2012?",
-        points: 15,
-        options: [
-          {
-            text: "getting trained as master of the mystic arts",
-            isRight: false
-          },
-          {
-            text: "performing surgery as a real doctor",
-            isRight: true
-          }
-        ]
-      }
-    ]
-  },
-  "Dc": {
-    quizName: "Marvel Cinematic Universe",
-    questions: [
-      {
-        question: "Who is Gotham’s Clown Prince of Crime?",
-        points: 5,
-        options: [
-          {
-            text: "Joker ",
-            isRight: true
-          },
-          {
-            text: "Harley Quinn ",
-            isRight: false
-          },
-          {
-            text: "Crazy Quilt ",
-            isRight: false
-          },
-          {
-            text: "Mad Hatter",
-            isRight: false
-          }
-        ]
-      }
-    ]
-  },
+
   "Agricultural And Rural Land Use" : {
     quizName: "Agricultural And Rural Land Use",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.JhF1BGWOMXVSN5Q8A3ABfQHaDp%26pid%3DApi&f=1",
     questions: [
       {
         question: " How have globalization and improved transportation links changed the geography of production areas for products such as apples, grapes, and fresh cut flowers? ",
@@ -114,10 +54,290 @@ export const quizdata: Quizdata = {
           }
         ]
       },
+      {
+        question: "The process by which people engaged in shifting cultivation plant crops of varying heights in order to protect lower crops is called",
+        points: 5,
+        options: [
+          {
+            text: "Subsistence agriculture",
+            isRight: false
+          },
+          {
+            text: "Shifting cultivation",
+            isRight: false
+          },
+          {
+            text: "Ridge tillage",
+            isRight: false
+          },
+          {
+            text: "Swidden agriculture",
+            isRight: false
+          },
+          {
+            text: "Intertillage",
+            isRight: true
+          }
+        ]
+      },
+
+      {
+        question: "Another name for truck farming is ",
+        points: 5,
+        options: [
+          {
+            text: "Market gardening",
+            isRight: false
+          },
+          {
+            text: "Specialty farming",
+            isRight: false
+          },
+          {
+            text: "Intensive commercial agriculture",
+            isRight: false
+          },
+          {
+            text: "Commercial gardening and fruit farming",
+            isRight: false
+          },
+          {
+            text: "All of the above",
+            isRight: true
+          }
+        ]
+      },
+      {
+        question: "In upland areas where plantation is present, what are the two most common crops grown? ",
+        points: 5,
+        options: [
+          {
+            text: "Tea and coffee",
+            isRight: true
+          },
+          {
+            text: "Coffee and sugarcane ",
+            isRight: false
+          },
+          {
+            text: "Tea and sugar cane ",
+            isRight: false
+          },
+          {
+            text: "Bananas and tea ",
+            isRight: false
+          },
+          {
+            text: "Sugarcane and bananas",
+            isRight: false
+          }
+        ]
+      },
+      {
+        question: "The land division system most responsible for dispersed rural settlements in many parts of the US is the",
+        points: 5,
+        options: [
+          {
+            text: "Enclosure movement",
+            isRight: false
+          },
+          {
+            text: "Metes and bounds system",
+            isRight: false
+          },
+          {
+            text: "Township and range system",
+            isRight: true
+          },
+          {
+            text: "Long lot system",
+            isRight: false
+          },
+          {
+            text: "None of the above",
+            isRight: false
+          }
+        ]
+      },
+      {
+        question: " All of the following are types of subsistence agriculture except ",
+        points: 5,
+        options: [
+          {
+            text: "Intensive rice farming",
+            isRight: false
+          },
+          {
+            text: "Pastoral nomadism",
+            isRight: false
+          },
+          {
+            text: "Swidden agriculture",
+            isRight: false
+          },
+          {
+            text: "Shifting cultivation",
+            isRight: false
+          },
+          {
+            text: "Truck farming",
+            isRight: true
+          }
+        ]
+      },
+    ]
+  },
+  "Food And Agriculture" : {
+    quizName: "Food And Agriculture",
+    image:"https://media.proprofs.com/images/QM/user_images/1826446/1484723559.jpg",
+    questions: [
+      {
+        question: "Plowing with machines, irrigating with drip systems and _____________ are all modern agricultural methods. ",
+        points: 5,
+        options: [
+          {
+            text: "Using manure",
+            isRight: false
+          },
+          {
+            text: "Applying chemical fertilizers",
+            isRight: true
+          },
+          {
+            text: "Irrigating with ditches ",
+            isRight: false
+          },
+          {
+            text: "All of the above",
+            isRight: false
+          }
+        ]
+      },
+      {
+        question: "Which of the following actions contributes to soil erosion? ",
+        points: 5,
+        options: [
+          {
+            text: "Using compost as fertilizer",
+            isRight: false
+          },
+          {
+            text: "Allowing land to lie fallow ",
+            isRight: false
+          },
+          {
+            text: "Driving farm machinery over fields",
+            isRight: true
+          },
+          {
+            text: "All of these practices contribute to soil erosion",
+            isRight: false
+          }
+        ]
+      },
+      {
+        question: "When a farmer uses no-till farming methods,",
+        points: 5,
+        options: [
+          {
+            text: "Seeds are planted among the roots of the previous crop",
+            isRight: true
+          },
+          {
+            text: "More erosion is likely ",
+            isRight: false
+          },
+          {
+            text: "Only organic fertilizers are used ",
+            isRight: false
+          },
+          {
+            text: "All of the above",
+            isRight: false
+          }
+        ]
+      },
+      {
+        question: "No-till farming helps to conserve soil fertility because ",
+        points: 5,
+        options: [
+          {
+            text: "Remnants of the previous crop are left to slowly decay",
+            isRight: true
+          },
+          {
+            text: "Deep ridges are cut across, not down, the slopes of hills ",
+            isRight: false
+          },
+          {
+            text: "The ground is carefully turned to mix soil nutrients",
+            isRight: false
+          },
+          {
+            text: "Only organic fertilizers and natural pesticides are used ",
+            isRight: false
+          }
+          
+        ]
+      },
+      {
+        question: "The results of the green revolution were not entirely positive because ",
+        points: 5,
+        options: [
+          {
+            text: "There were too few subsistence farmers ",
+            isRight: false
+          },
+          {
+            text: "Use of pesticides and fertilizers increased the risk of population ",
+            isRight: true
+          },
+          {
+            text: "Substantial resources were no longer required to grow the new grain varieties ",
+            isRight: false
+          },
+          {
+            text: "All of the above ",
+            isRight: false
+          }
+        ]
+      },
     ]
   }
 };
-
+// "" : {
+//   quizName: "",
+//   questions: [
+//     {
+//       question: "",
+//       points: 5,
+//       options: [
+//         {
+//           text: "",
+//           isRight: false
+//         },
+//         {
+//           text: " ",
+//           isRight: false
+//         },
+//         {
+//           text: " ",
+//           isRight: false
+//         },
+//         {
+//           text: " ",
+//           isRight: false
+//         },
+//         {
+//           text: "",
+//           isRight: true
+//         }
+//       ]
+//     },
+ 
+//   ]
+// }
+// };
 // const quizOne: quiz = {
 //   quizName: "Marvel Cinematic Universe",
 //   questions: [
